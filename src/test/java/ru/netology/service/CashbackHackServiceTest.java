7f44a5a8d7bbed1a1e7107e0405f59ea10f529af
MaratIsashev <isashevmarat@gmail.com>
2023-08-17 21:17:30 +0300

    CI added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,17 +1,26 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.Assert.*;
 
 class CashbackHackServiceTest {
 
     @Test
     void remainMoreThanZero() {
         int amount = 900;
-        int expected = 10;
+        int expected = 100;
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(amount);
-        assertEquals(expected,actual);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void remainIsZero() {
+        int amount = 9000;
+        int expected = 0;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
     }
 }
